29703b621902e26868c654c6977058f118a8714e
ERAKALA SAI TEJA <91062397+SaiTeja0501@users.noreply.github.com>
2022-04-26 15:33:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ class Heart_Form(FlaskForm):
 
     # Fields for Graphs
     name = StringField('Patient\'s Name',[validators.DataRequired(),Length(min=3, max=20)])
-    age = FloatField('Age',[validators.DataRequired(),Length(min=1, max=130)])
+    age = FloatField('Age')
     trestbps = FloatField('Resting Systolic Blood Pressure (mm Hg)')
     chol = FloatField('Serum Cholesterol Levels (mg/dl)')
     thalach = FloatField('Maximum Heart Rate Achieved')
